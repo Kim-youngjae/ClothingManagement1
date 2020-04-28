91a489781b9d7ed7fc2bc52207a729322b27012d
user <user@DESKTOP-2KR5IV5>
2020-04-28 13:49:02 +0900

    remodified on 4/28

--- src/clothing/HeadWears.java
+++ src/clothing/HeadWears.java
@@ -9,33 +9,35 @@ public class HeadWears extends Clothing {
 	
 	public void getUserInput(Scanner input) {
 
-		System.out.print("가방 종류가 무엇인가요? :");
+		System.out.print("모자 종류가 무엇인가요? :");
 		String name = input.next();
 		this.setName(name);
 
 		char answer = 'x';
 		while(answer != 'y' && answer != 'Y' && answer != 'n' && answer != 'N') {
-			System.out.println("정말 본인의 가방 입니까? (Y/N) :");
+			System.out.println("정말 본인의 모자 입니까? (Y/N) :");
 			answer =input.next().charAt(0);
 			if(answer == 'Y' || answer == 'y') {
-				System.out.print("누구의 가방입니까? :");
+				System.out.print("누구의 모자입니까? :");
 				String whos = input.next();
 				this.setWhos(whos);
 				break;
 			}
 			else if(answer == 'n' || answer == 'N') {
-				this.setWhos("");
+				System.out.print("누구의 모자입니까? :");
+				String whos = input.next();
+				this.setWhos(whos);
 				break;
 			}
 			else {
 			}
 		}
 
-		System.out.print("어느 계절에 적합한 가방입니까? :");
+		System.out.print("어느 계절에 적합한 모자입니까? :");
 		String weather = input.next();
 		this.setWeather(weather);
 
-		System.out.print("가방 관리시 주의 사항 :");
+		System.out.print("모자 관리시 주의 사항 :");
 		String warning = input.next();
 		this.setWarning(warning);
 	}
